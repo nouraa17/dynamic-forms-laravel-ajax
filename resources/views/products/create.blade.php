9750c2b3a921dd5982f31833b3e6a61322475663
nouraa17 <nouranana31@gmail.com>
2024-09-25 02:02:31 +0300

    first committ

--- resources/views/products/create.blade.php
+++ resources/views/products/create.blade.php
@@ -47,6 +47,8 @@
                     </div>
                 </form>
         </div>
+
+        <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
         <script>
             $(document).ready(function() {
                 $('#category_id').change(function() {
